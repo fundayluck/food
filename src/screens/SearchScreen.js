51563f8b1e038b9f715135b24c0a31a678c2dee6
jember08 <afandayul@gmail.com>
2022-05-10 02:17:42 +0700

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
