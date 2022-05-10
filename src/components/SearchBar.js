4b1f1dd9a86772d87ce8c349de9ccc7b769cfece
jember08 <afandayul@gmail.com>
2022-05-11 03:26:22 +0700

    fix package and managing state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from "react";
 import { TextInput, View, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.background}>
       <Feather style={styles.iconStyle} name="search" />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+      />
     </View>
   );
 };
