83bc55a32c7a2dc3bf38af1f18bf693300d40392
jember08 <afandayul@gmail.com>
2022-05-11 15:35:31 +0700

    making the request

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -7,5 +7,3 @@ export default axios.create({
       "bearer vvw6cKEq-jW9R0d0yEEW1oB6t2RbbaeVmLRyD_qSIVmaLVe52LiNO6sh3XVKzBDWqnsM5O58Y6I5UPtI3xX4Nkn3zgd2gmEhasWFPtWFfeNJd_qrKkLFM90huzx5YnYx",
   },
 });
-
-yelp.get("/search");
