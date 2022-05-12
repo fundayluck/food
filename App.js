28ddc4f669b9a417e5df7639dc67a4b6d31a1b46
jember08 <afandayul@gmail.com>
2022-05-12 21:43:35 +0700

    navigation

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "./src/screens/SearchScreen";
+import DetailShowScreen from "./src/screens/DetailShowScreen";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
+    Detail: DetailShowScreen,
   },
   {
     initialRouteName: "Search",
---
+++ src/screens/DetailShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const DetailShowScreen = () => {
+  return (
+    <View>
+      <Text>Detail Show</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default DetailShowScreen;
